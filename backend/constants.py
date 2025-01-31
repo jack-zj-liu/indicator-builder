4377079f557b9e955221177e7b3b148bf238d6f9
Charlie Cao <charliecao0207@gmail.com>
2025-01-31 13:49:01 -0500

    implement redis cache

--- backend/constants.py
+++ backend/constants.py
@@ -1,3 +1,5 @@
 BACKEND_URL = "http://127.0.0.1:8000"
 FRONTEND_URL = 'http://localhost:5173'
 ASSETS_FILE_PATH = "assets.txt"
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import BackgroundTasks, FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 import requests
 from datetime import datetime, timedelta
@@ -6,8 +6,19 @@ from fastapi.middleware.cors import CORSMiddleware
 import yfinance as yf
 import pandas as pd
 import os
+import logging
+import uvicorn
+import json
 
 import constants
+try:
+    import redis
+    r = redis.Redis(host=constants.REDIS_HOST, port=constants.REDIS_PORT, decode_responses=True)
+    REDIS_EXPIRY = 60 * 30 # 30 minutes (arbitrary idk)
+    r.ping()
+    redis_available = True
+except:
+    redis_available = False
 
 
 app = FastAPI()
@@ -18,6 +29,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+LOG = logging.getLogger('uvicorn.error')
+LOG.setLevel(logging.DEBUG)
 
 
 @app.get("/")
@@ -25,41 +38,74 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/time_series_daily/{symbol}")
-async def get_time_series_daily(symbol):
-    ticker = yf.Ticker(symbol)
+def insert_redis_data(redis_name, data):
+    # serialize each dict as a string and insert into redis
+    for entry in data:
+        redis_entry = json.dumps(entry)
+        r.rpush(redis_name, redis_entry)
+    r.expire(redis_name, REDIS_EXPIRY)
+    inserted = r.llen(redis_name)
+    # for debug, remove later?
+    LOG.debug('Redis: inserted {} items into \'{}\''.format(inserted, redis_name))
 
-    data = ticker.history(period='max', interval='1d').Close
+def get_series_data_redis(symbol, interval, background_tasks: BackgroundTasks):
+    ticker = yf.Ticker(symbol)
+    redis_name = ':'.join([symbol, interval])
+
+    # load data from redis if exists
+    if r.exists(redis_name):
+        formatted_data = list(map(json.loads, r.lrange(redis_name, 0, -1)))
+        # for debug, remove later?
+        retrieved = r.llen(redis_name)
+        LOG.debug('Redis: retrieved {} items from \'{}\''.format(retrieved, redis_name))
+    else:
+        data = ticker.history(period='max', interval=interval).Close
+        formatted_data = [
+            {"time": pd.Timestamp(date).strftime('%Y-%m-%d'), "value": round(value, 2)}
+            for date, value in data.items()
+        ]
+        # do redis insert in background cus it might take a bit
+        background_tasks.add_task(insert_redis_data, redis_name, formatted_data)
+
+    return formatted_data
+
+def get_series_data(symbol, interval):
+    ticker = yf.Ticker(symbol)
+    data = ticker.history(period='max', interval=interval).Close
     formatted_data = [
         {"time": pd.Timestamp(date).strftime('%Y-%m-%d'), "value": round(value, 2)}
         for date, value in data.items()
     ]
+
+    return formatted_data
+
+
+@app.get("/time_series_daily/{symbol}")
+async def get_time_series_daily(symbol, background_tasks: BackgroundTasks):
+    if redis_available:
+        formatted_data = get_series_data_redis(symbol, '1d', background_tasks)
+    else:
+        formatted_data = get_series_data(symbol, '1d')
 
     return formatted_data
 
 
 @app.get("/time_series_weekly/{symbol}")
-async def get_time_series_weekly(symbol):
-    ticker = yf.Ticker(symbol)
-
-    data = ticker.history(period='max', interval='1wk').Close
-    formatted_data = [
-        {"time": pd.Timestamp(date).strftime('%Y-%m-%d'), "value": round(value, 2)}
-        for date, value in data.items()
-    ]
+async def get_time_series_weekly(symbol, background_tasks: BackgroundTasks):
+    if redis_available:
+        formatted_data = get_series_data_redis(symbol, '1wk', background_tasks)
+    else:
+        formatted_data = get_series_data(symbol, '1wk')
 
     return formatted_data
 
 
 @app.get("/time_series_monthly/{symbol}")
-async def get_time_series_monthly(symbol):
-    ticker = yf.Ticker(symbol)
-
-    data = ticker.history(period='max', interval='1mo').Close
-    formatted_data = [
-        {"time": pd.Timestamp(date).strftime('%Y-%m-%d'), "value": round(value, 2)}
-        for date, value in data.items()
-    ]
+async def get_time_series_monthly(symbol, background_tasks: BackgroundTasks):
+    if redis_available:
+        formatted_data = get_series_data_redis(symbol, '1mo', background_tasks)
+    else:
+        formatted_data = get_series_data(symbol, '1mo')
 
     return formatted_data
 
@@ -83,4 +129,3 @@ async def list_assets():
         return JSONResponse(content={"assets": assets_map})
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error reading assets file: {str(e)}")
-
